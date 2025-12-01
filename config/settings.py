c75d431e1c92c30391759c3176274f36e5f2b010
Stuture <info@stuture.ch>
2025-12-01 15:05:59 +0100

    Remove local staticfiles

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!import os
-SECRET_KEY = os.environ.get("django-insecure-s8-r#vc$pnvxgq07tvi00vgyu_lo3k%0%w_!1k%#k*%6xoc)79")
+SECRET_KEY = os.environ.get(
+    "DGJ4pVqX!%s4n9v#j3@8mQ$k2&uH7rZg^Wb0Fc*LpOEhT1yXw",
+    "django-insecure-s8-r#vc$pnvxgq07tvi00vgyu_lo3k%0%w_!1k%#k*%6xoc)79"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -119,12 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
