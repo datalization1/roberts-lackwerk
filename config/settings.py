c0a9c22f8d420aa344be2f08c83efe2e8bad0697
Stuture <info@stuture.ch>
2025-12-01 11:20:33 +0100

    Prepare production settings for Heroku

--- config/settings.py
+++ config/settings.py
@@ -20,13 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-s8-r#vc$pnvxgq07tvi00vgyu_lo3k%0%w_!1k%#k*%6xoc)79"
+# SECURITY WARNING: keep the secret key used in production secret!import os
+SECRET_KEY = os.environ.get("django-insecure-s8-r#vc$pnvxgq07tvi00vgyu_lo3k%0%w_!1k%#k*%6xoc)79")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "roberts-lackwerk.ch",
+    "www.roberts-lackwerk.ch",
+    ".herokuapp.com",
+]
 
 
 # Application definition
@@ -44,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -113,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [ BASE_DIR / "static" ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
