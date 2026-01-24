c798abc4de566a03b136a3cfd940a0a03f15d5e2
Datalization <info@infodatalization.onmicrosoft.com>
2026-01-24 10:26:31 +0100

    Fix S3 upload ACL for Heroku

--- config/settings.py
+++ config/settings.py
@@ -210,7 +210,7 @@ if os.getenv("AWS_STORAGE_BUCKET_NAME"):
     AWS_S3_CUSTOM_DOMAIN = os.getenv("AWS_S3_CUSTOM_DOMAIN") or None
     AWS_S3_FORCE_PATH_STYLE = os.getenv("AWS_S3_FORCE_PATH_STYLE", "False") == "True"
     AWS_QUERYSTRING_AUTH = False  # öffentliche URLs ohne Signatur
-    AWS_DEFAULT_ACL = "public-read"
+    AWS_DEFAULT_ACL = None
     AWS_S3_FILE_OVERWRITE = False
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "86400"}
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
